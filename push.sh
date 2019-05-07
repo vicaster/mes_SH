0e7fea4ab737e72f07b6b6cdce45de35b057c70d
Victor-louis Castera <vicaster@z4r6p4.le-101.fr>
2019-05-07 19:26:26 +0200

    Tue May  7 19:26:26

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
 find . -name 'a.out' -print -delete | find . -name '.DS_Store' -print -delete | echo "\nKill The Cash\n\n"
-git add .
-git commit -m "vic"
-git push
+git add . && git commit -m "`date | cut -d ' ' -f1,2,3,4,5`" && git push
 echo "\n\n------------------------------\n\n"
 git ls-files
